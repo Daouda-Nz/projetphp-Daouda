82515f6f8b76d72241b634b81bc1667cf67f8af9
Daouda Diakhaté <diakhatedaouda07@icloud.com>
2026-04-25 23:28:37 +0000

    update project

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+class Personne {
+    private $id;
+    private $nom;
+    private $email;
+
+    public function __construct($id, $nom, $email) {
+    $this->id = $id;
+    $this->nom = $nom;
+    $this->email = $email;
+   }
+
+   public function afficherInfos() {
+    return "ID: " . $this->id . ", Nom: " . $this->nom . ", Email: " . $this->email;
+    }
+
+}
+
+$p = new Personne(1, "Ali", "ali@mail.com");
+
+echo $p->afficherInfos();
+
+?>
+
